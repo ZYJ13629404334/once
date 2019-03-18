69bd29840cd0dc4d804bf88010bc4a224f526ef7
ZYJ <2918983320@qq.com>
2019-03-18 20:17:09 +0800

    header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
  <div>
-  左玉娟，2019-3-18你好 !
+  <home-header></home-header>
  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default{
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
