7f0599ba3ca60488107dc74830251ef988bc422b
ZYJ <2918983320@qq.com>
2019-03-18 18:34:09 +0800

    zyj

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
 
